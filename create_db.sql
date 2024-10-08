156975b66f69cc8c34e09b019ee2e830514b9704
Spike <spikeelliot+1@gmail.com>
2024-10-08 16:03:34 +0100

    Finished code for lab 2
    Added log in functionality, user list, and hashed passwords

--- create_db.sql
+++ create_db.sql
@@ -1,12 +1,53 @@
-# Create database script for Bettys books
+-- MySQL Workbench Forward Engineering
 
-# Create the database
-CREATE DATABASE IF NOT EXISTS bettys_books;
-USE bettys_books;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-# Create the tables
-CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema bettys_books
+-- -----------------------------------------------------
 
-# Create the app user
-CREATE USER IF NOT EXISTS 'bettys_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
-GRANT ALL PRIVILEGES ON bettys_books.* TO ' bettys_books_app'@'localhost';
+-- -----------------------------------------------------
+-- Schema bettys_books
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `bettys_books` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `bettys_books` ;
+
+-- -----------------------------------------------------
+-- Table `bettys_books`.`books`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bettys_books`.`books` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NULL DEFAULT NULL,
+  `price` DECIMAL(5,2) UNSIGNED NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `bettys_books`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bettys_books`.`users` (
+  `userid` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(50) NULL DEFAULT NULL,
+  `hashedPassword` VARCHAR(100) NULL DEFAULT NULL,
+  `firstname` VARCHAR(50) NULL DEFAULT NULL,
+  `lastname` VARCHAR(50) NULL DEFAULT NULL,
+  `email` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`userid`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 // Create a new router
 const express = require("express")
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const router = express.Router()
 
 router.get('/register', function (req, res, next) {
@@ -8,7 +10,63 @@ router.get('/register', function (req, res, next) {
 
 router.post('/registered', function (req, res, next) {
     // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email)                                                                           
+    const plainPassword = req.body.password;
+    bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
+        let sqlquery = "INSERT INTO users (username, hashedPassword, firstName, lastName, email) VALUES (?,?,?,?,?)";
+        let newrecord = [req.body.username, hashedPassword, req.body.first, req.body.last, req.body.email];
+        db.query(sqlquery, newrecord, (err, result) => {
+            if (err) {
+                next(err);
+            }
+            else {
+                result = `Hello ${req.body.first} ${req.body.last} you are now registered!  
+                We will send an email to you at ${req.body.email} `;
+                result += `Your password is: ${plainPassword} and your hashed password is: ${hashedPassword}`;
+                res.send(result);
+            }
+        })
+    });                                                                      
+})
+
+router.get('/login', function (req, res, next) {
+    res.render('login.ejs');                                                        
+})
+
+router.post('/loggedin', function (req, res, next) {
+    // saving data in database
+    let sqlquery = `SELECT hashedPassword FROM users WHERE username = '${req.body.username}'` // query database to get all the books
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            next(err);
+        }
+        if (result.length <= 0) {
+            res.send("Error: user not found");
+            return;
+        } 
+        hashedPassword = result[0].hashedPassword;
+        bcrypt.compare(req.body.password, hashedPassword, function(err, result) {
+            if (err) {
+                res.send("Error: user not found");
+            }
+            else if (result) {
+                res.send("Successfully logged in");
+            }
+            else {
+                res.send("Incorrect password");
+            }
+        })
+    })
+});                                                                      
+
+router.get('/list', function (req, res, next) {
+    let sqlquery = "SELECT * FROM users" // query database to get all the books
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            next(err)
+        }
+        res.render("userlist.ejs", {users:result})
+     })
 })
 
 // Export the router object so index.js can access it
